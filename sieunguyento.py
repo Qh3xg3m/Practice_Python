30c7d8dfeef7e19c14abbf492f2052356039633b
Qh3xg3m <nguyenquytdnb@gmail.com>
2020-01-18 23:01:43 +0700

    sieunguyento

---
+++ sieunguyento.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def nto(x):
+	if x < 2:
+		lg = False
+	else:
+		lg = True
+		for uoc in range(2,x):
+			if x % uoc == 0:
+				lg = False
+	return(lg)
+
+def main(x):
+	lg = True
+	if nto(x):
+		i = x
+		while x != 0:
+			if nto(x)==False:
+				lg=False
+			x = x // 10
+		if lg == True:
+			print(i)
+
+for x in range(10001):
+	main(x)
+
+
