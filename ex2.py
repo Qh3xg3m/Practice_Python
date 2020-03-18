171c23bbeb86bfea52bd5d35272095c2b43c36b2
Qh3xg3m <nguyenquytdnb@gmail.com>
2020-03-18 20:34:14 +0700

    day2

--- ex2.py
+++ ex2.py
@@ -21,3 +21,6 @@ the new array is the product of all the numbers in the original array except the
 
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be 
 [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6]."""
+
+
+
